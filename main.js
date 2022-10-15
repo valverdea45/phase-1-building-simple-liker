800251a92ff86502fa9bd29e1f381c4db104cade
Adrian <adrianvalverde45@gmail.com>
2022-10-15 16:50:54 -0400

    commit tree 3fd6633848201a005534ad105a86b7e8cbbdbe02

--- main.js
+++ main.js
@@ -4,7 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let errorMessage = document.querySelector("div#modal")
+errorMessage.classList.add("hidden");
 
+ let heartButton = document.querySelectorAll(".like-glyph")
+ for(const element of heartButton) {
+  element.addEventListener("click", likeClick)
+ }
+ function likeClick(e){
+  const heart = e.target
+  if (heart.innerHTML === FULL_HEART){
+    heart.classList.remove("activated-heart")
+    heart.innerHTML = EMPTY_HEART
+  } else {
+  mimicServerCall()
+  .then(() => {
+    heart.innerHTML = FULL_HEART
+    heart.classList.add("activated-heart")
+  })
+  .catch((result) => {
+    errorMessage.classList.remove("hidden")
+    let errorMessage2 = document.querySelector("#modal-message")
+    errorMessage2.innerHTML = result
+    setTimeout(() => remove(), 3000)
+  })
+ }}
+
+function remove(){
+  errorMessage.classList.add("hidden");
+}
 
 
 //------------------------------------------------------------------------------
